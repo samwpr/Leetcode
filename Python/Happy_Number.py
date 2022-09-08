2be774030f0e46cc24c6e61a735ab899f862ef65
samwpr <swpr10x@gmail.com>
2022-09-08 12:12:45 +0800

    New Problem: Happy Numbers

---
+++ Python/Happy_Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    #@snoop
+    def isHappy(self, n: int) -> bool:
+        past = set() 
+        while n != 1:
+            n = sum(int(i)**2 for i in str(n))
+            if n in past:
+                return False
+            past.add(n)
+        return True
+
+test1 = Solution()
+print(test1.isHappy(19))
