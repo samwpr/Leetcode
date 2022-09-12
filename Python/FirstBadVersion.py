308779a74cbb0e10fd0e89b4fc770916abfcc1ac
samwpr <swpr10x@gmail.com>
2022-09-12 12:17:21 +0800

    Added First Bad Version (Lvl: Easy, Com: Google)

--- Python/FirstBadVersion.py
+++ Python/FirstBadVersion.py
@@ -1,6 +1,25 @@
+# https://leetcode.com/problems/first-bad-version/
 
-def number(n: int):
-    x = list(range(0, n))
-    print(x)
+bad = 0
+def isBadVersion(version):
+    if version >= bad:
+        return True
+    return False
 
-number(5)
+def firstBadVersion(n):
+    if n < 2:
+        return n
+    start = 1
+    end = n
+    while (start <= end):
+        mid = (start + end)//2
+        if isBadVersion(mid) == True and isBadVersion(mid-1) == False:
+            return mid
+        elif isBadVersion(mid-1) == True:
+            end = mid -1
+        else:
+            start = mid+1
+
+bad = 4
+print(firstBadVersion(5))
+
