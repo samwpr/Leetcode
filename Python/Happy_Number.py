111387eb9c134bec1d4bf4b7daaf95604ce35dad
samwpr <swpr10x@gmail.com>
2022-09-13 10:31:12 +0800

    Reattempting Happy Number

--- Python/Happy_Number.py
+++ Python/Happy_Number.py
@@ -1,19 +1,41 @@
-class Solution:
+class Solution(object):
     #@snoop
     def isHappy(self, n: int) -> bool:
-        past = set() 
+        pastNum = set() #using set as all values need to be unique to ensure no infinite loop
         while n != 1:
             n = sum(int(i)**2 for i in str(n))
-            if n in past:
+            if n not in pastNum:
+                pastNum.add(n)
+            else: 
                 return False
-            past.add(n)
         return True
 
-
         
+obj = Solution()
+print(obj.isHappy(19))
+print(obj.isHappy(2))
 
-test1 = Solution()
-print(test1.isHappy(19))
-print(test1.isHappy2(19))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
