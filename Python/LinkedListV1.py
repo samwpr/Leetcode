0344b2ce25b981b26624c7bc9ffebb6cea79adef
samwpr <swpr10x@gmail.com>
2022-09-14 11:30:44 +0800

    Updated LinkedList

--- Python/LinkedListV1.py
+++ Python/LinkedListV1.py
@@ -1,11 +1,28 @@
 class Node(object):
-    def __init__(self, data, node = None):
-        self.data = data
+    def __init__(self, input: int, node = None):
+        self.data = input
         self.next_node = node
 
 class LinkedList(object):
-    def __init__(self):
-        self.root = None
+    def __init__(self, rootNode = None):
+        self.root = rootNode
+        self.size = 0
+    
+    def add(self, input: int):
+        NEW_node = Node(input, self.root)
+        self.root = NEW_node
+        self.size += 1
 
-    def add(self, input):
-        pass
+    def printList(self):
+        temp = self.root
+        while temp != None:
+            print(temp.data)
+            temp = temp.next_node
+        print("")
+
+myList = LinkedList()
+myList.add(5)
+myList.add(8)
+myList.printList() 
+#8
+#5
---
+++ Python/Snapshot_Array(m-Google).py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/snapshot-array/
+
+'''
+Input: ["SnapshotArray","set","snap","set","get"]
+[[3],[0,5],[],[0,6],[0,0]]
+Output: [null,null,0,null,5]
+Explanation: 
+SnapshotArray snapshotArr = new SnapshotArray(3); // set the length to be 3
+snapshotArr.set(0,5);  // Set array[0] = 5
+snapshotArr.snap();  // Take a snapshot, return snap_id = 0
+snapshotArr.set(0,6);
+snapshotArr.get(0,0);  // Get the value of array[0] with snap_id = 0, return 5
+'''
+
+from collections import defaultdict
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.snap_id = 0
+        self.history = defaultdict(dict)
+        
+    def set(self, index: int, val: int) -> None:
+        self.history[self.snap_id][index] = val
+        
+    def snap(self) -> int:
+        self.snap_id += 1
+        return self.snap_id-1
+
+    def get(self, index: int, snap_id: int) -> int:
+        for i in range(snap_id,-1,-1):
+            if index in self.history[i]:
+                return self.history[i][index]
+        return 0 # default value in case it wasn't set earlier
+
+ob = SnapshotArray(3) 
+ob.set(0, 5)
+print(ob.snap()) #Print 0
+ob.set(0,6)
+print(ob.get(0,0)) #Print 5 
+        
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
+
