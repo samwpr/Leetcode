fd238b47b6e06bebacaa0c8f550949af5c42b88f
samwpr <swpr10x@gmail.com>
2022-09-16 12:50:19 +0800

    Reattempt First Bad Version

--- Python/FirstBadVersion.py
+++ Python/FirstBadVersion.py
@@ -1,25 +1,34 @@
 # https://leetcode.com/problems/first-bad-version/
 
+
 bad = 0
-def isBadVersion(version):
+def isBadVersion(version: int) -> bool:
     if version >= bad:
         return True
     return False
 
-def firstBadVersion(n):
-    if n < 2:
+def firstBadVersion(n: int) -> int:
+    if n < 2: 
         return n
-    start = 1
-    end = n
-    while (start <= end):
-        mid = (start + end)//2
-        if isBadVersion(mid) == True and isBadVersion(mid-1) == False:
+    
+    left = 1 
+    right = n
+
+
+    while (left <= right):
+        mid = (left + right) // 2 
+        if isBadVersion(mid) == True and isBadVersion(mid -1) == False:
             return mid
-        elif isBadVersion(mid-1) == True:
-            end = mid -1
-        else:
-            start = mid+1
+        elif isBadVersion(mid - 1) == True:
+            right = mid -1
+            #print("1", mid)
+        else: 
+            left = mid + 1 
+            #print("2", mid)
 
 bad = 4
 print(firstBadVersion(5))
+
+bad = 1
+print(firstBadVersion(1))
 
