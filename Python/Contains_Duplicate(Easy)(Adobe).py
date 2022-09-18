15e0a26f67a316be7eacd07c9200abc5f6d79d8d
samwpr <swpr10x@gmail.com>
2022-09-18 15:23:11 +0800

    reattempt: Contains Duplicate

--- Python/Contains_Duplicate(Easy)(Adobe).py
+++ Python/Contains_Duplicate(Easy)(Adobe).py
@@ -1,10 +1,11 @@
 #https://leetcode.com/problems/contains-duplicate/
+#Easy | Adobe
 
 '''
 Input: nums = [1,2,3,1]
 Output: true
 
-Input: nums = [1,2,3,4]
+Input: c
 Output: false
 
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
@@ -14,15 +15,17 @@ Output: true
 class Solution:
     def containsDuplicate(self, nums: list) -> bool:
         unique = set()
-        for i in nums:
-            if i not in unique:
-                unique.add(i)
-            else: 
+        for i in nums: 
+            if i in unique:
                 return True
+            else:
+                unique.add(i)
         return False
-            
 
 obj = Solution()
-nums = [1, 2, 3, 4]
-nums = [1, 1, 2, 3, 4]
+nums = [1,2,3,1]
+print(obj.containsDuplicate(nums))
+nums = [1,2,3,4]
+print(obj.containsDuplicate(nums))
+nums = [1,1,1,3,3,4,3,2,4,2]
 print(obj.containsDuplicate(nums))
