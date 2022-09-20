f5b426503f1e614ae204c9ef26a58b829a9dc7bf
samwpr <swpr10x@gmail.com>
2022-09-20 14:12:19 +0800

    Understanding how to reverse LinkedList

--- Python/LinkedListV1.py
+++ Python/LinkedListV1.py
@@ -1,28 +1,33 @@
-class Node(object):
+class Node:
     def __init__(self, input: int, node = None):
-        self.data = input
+        self.data = input 
         self.next_node = node
 
-class LinkedList(object):
-    def __init__(self, rootNode = None):
-        self.root = rootNode
-        self.size = 0
-    
+class LinkedList:
+    def __init__(self, root_node = None): #Is there a need for root_node to be a parameter? 
+        self.root = root_node
+        self.size = 0 
+
     def add(self, input: int):
-        NEW_node = Node(input, self.root)
-        self.root = NEW_node
+        new_node = Node(input, self.root)
+        self.root = new_node
         self.size += 1
 
-    def printList(self):
+    def print(self):
         temp = self.root
-        while temp != None:
-            print(temp.data)
+        while temp != None: 
+            print(temp.data, " ")
             temp = temp.next_node
-        print("")
+
 
 myList = LinkedList()
-myList.add(5)
-myList.add(8)
-myList.printList() 
-#8
-#5
+myList.add(1)
+myList.add(2)
+myList.add(3)
+myList.print()
+
+#What is end=" "
+
+
+
+
--- Python/Reverse_LinkedList.py
+++ Python/Reverse_LinkedList.py
@@ -22,6 +22,7 @@ class LinkedList():
     
     def add(self, input: int):
         new_node = ListNode(input, self.root)
+        print(input, new_node)
         self.root = new_node
         self.size += 1
 
@@ -32,17 +33,74 @@ class LinkedList():
             temp = temp.next_node
         print("")
 
-    def reverse_Iterative(self):
-        prev = None 
-        current = self.root
-        while current != None:
-            next_node = current.next_node
-            current.next_node = prev
-            prev = current
-            current = next_node
-        self.root = prev
 
-    '''
+    def reverse(self):
+        a = self.root
+        b = None #This is not needed to declare 
+        c = None 
+
+        while a != None: 
+            b = a
+            a = a.next_node
+            b.next_node = c
+            c = b 
+        self.root = b
+
+        
+
+
+    def reverse_Iterative(self):
+        prev = None
+        current = self.root
+        print("current = self.root", current, "\n")
+        
+        while current != None:
+            #print("current =", current)
+            next_node = current.next_node
+            print("next node = current.next_node", current.next_node, "\n")
+
+            current.next_node = prev
+            print("current.next_node = prev", current.next_node, prev, "\n")
+
+            prev = current
+            print("prev = current", prev, current, "\n")
+
+            current = next_node
+            print("current = next_node", current, next_node, "\n")
+
+        self.root = prev
+        print("self.root = prev", self.root, prev)
+
+
+
+'''
+    def reverse_Iterative(self):
+        prev = None
+        current = self.root
+        print("current = self.root", current, "\n")
+     
+        while current != None:
+            #print("current =", current)
+            next_node = current.next_node
+            print("next node = current.next_node", current.next_node, "\n")
+
+            current.next_node = prev
+            print("current.next_node = prev", current.next_node, prev, "\n")
+
+            prev = current
+            print("prev = current", prev, current, "\n")
+
+            current = next_node
+            print("current = next_node", current, next_node, "\n")
+
+        self.root = prev
+        print("self.root = prev", self.root, prev)
+
+
+
+
+
+
     def reverse_Recurvise(self, rootNode: ListNode):
         if rootNode == None or rootNode.next_node == None:
             return rootNode
@@ -52,16 +110,19 @@ class LinkedList():
         return current
     '''
 
-
 obj = LinkedList()
-obj.add(1)
-obj.add(2)
-obj.add(3)
+obj.add(5)
+#obj.print()
+obj.add(8)
 obj.print()
-obj.reverse_Iterative()
+#print("")
+#obj.add(3)
+#obj.print()
+#obj.reverse_Iterative()
+obj.reverse()
 obj.print()
-obj.reverse_Recurvise()
-obj.print()
+#obj.reverse_Recurvise()
+#obj.print()
 
 
 
