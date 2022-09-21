d2a65c0a5d8583fd1a27f5de9a9114d5665489b6
samwpr <swpr10x@gmail.com>
2022-09-21 12:27:33 +0800

    Reattempt Linked List

--- Python/LinkedListV1.py
+++ Python/LinkedListV1.py
@@ -1,33 +1,48 @@
+from audioop import add
+
+
 class Node:
     def __init__(self, input: int, node = None):
         self.data = input 
-        self.next_node = node
+        self.next = node 
 
-class LinkedList:
-    def __init__(self, root_node = None): #Is there a need for root_node to be a parameter? 
-        self.root = root_node
+class SinglyLinkedList:
+    def __init__(self, root_node = None):
+        self.head = root_node
         self.size = 0 
 
-    def add(self, input: int):
-        new_node = Node(input, self.root)
-        self.root = new_node
-        self.size += 1
+    def add(self, input: int): 
+        new_node = Node(input, self.head)
+        self.head = new_node 
+        self.size += 1 
 
     def print(self):
-        temp = self.root
-        while temp != None: 
-            print(temp.data, " ")
-            temp = temp.next_node
+        temp_node = self.head 
+        while temp_node != None: 
+            print(temp_node.data, end="")
+            temp_node = temp_node.next 
+        print("")
+
+    def reverse(self):
+        a = self.head
+        b = None 
+        c = None 
+        while a != None: 
+            b = a
+            a = a.next
+            b.next = c
+            c = b
+        self.head = b
 
 
-myList = LinkedList()
+
+
+myList = SinglyLinkedList()
+myLis2 = SinglyLinkedList()
 myList.add(1)
 myList.add(2)
 myList.add(3)
 myList.print()
-
-#What is end=" "
-
-
-
+myList.reverse()
+myList.print()
 
