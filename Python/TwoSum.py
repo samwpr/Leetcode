101494d96791b37d1ee51d5debde07f7eee8b23c
samwpr <swpr10x@gmail.com>
2022-09-24 16:08:05 +0800

    Reattempt: Two Sum

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -1,17 +1,22 @@
-#https://leetcode.com/problems/two-sum/
+'''
+Link: https://leetcode.com/problems/two-sum/
+Level: Easy
+Companines: Google, Accenture, Adobe, Amazon, Amex, Apple, Bloomberg, Dell, Expedia, Goldman, IBM, Intel, Intuit, JP, MicroSoft, Morgan Stanley, Orcale, Paypal, Salesforce, Samsung, Spotify, Uber, Visa, Walmart, Yahoo, Zillow, Zoho, Zomato, Zoom
+'''
 
 class Solution(object):
     def twoSum(self, nums: list, target: int) -> list:
-        numDict = {}
+        dictionary = {}
         for index, element in enumerate(nums):
             diff = target - element
-            if diff in numDict.keys():
-                return [numDict[diff], index]
-            numDict[element] = index
+            if diff in dictionary.keys():
+                return [dictionary[diff], index]
+            else: 
+                dictionary[element] = index
+
 
 obj = Solution()
-nums = [2, 7 ,11, 15]
+nums = [2, 7, 11, 15]
 target = 9
 print(obj.twoSum(nums, target))
-
 
