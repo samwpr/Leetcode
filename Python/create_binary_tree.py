f3e5bd86472b33e2dfbcac9308bb019769a91470
samwpr <swpr10x@gmail.com>
2022-09-28 15:27:33 +0800

    Added print tree by level

--- Python/create_binary_tree.py
+++ Python/create_binary_tree.py
@@ -56,6 +56,41 @@ class Node:
             res.append(binary_tree.data)
         return res
 
+
+    def printLevelOrder(self, binary_tree):
+        if binary_tree is None: 
+            return 
+        queue = []
+
+        queue.append(binary_tree)
+
+        while(len(queue) > 0):
+            print(queue[0].data, end=" ")
+            binary_tree = queue.pop(0)
+
+            if binary_tree.left is not None: 
+                queue.append(binary_tree.left)
+            
+            if binary_tree.right is not None:
+                queue.append(binary_tree.right)
+
+    def printLevelOrder2(self, binary_tree):
+        if binary_tree is None: 
+            return 
+        queue = []
+
+        queue.append(binary_tree)
+
+        while(len(queue) > 0):
+            print(queue[0].data, end=" ")
+            binary_tree = queue.pop(0)
+
+            if binary_tree.right is not None:
+                queue.append(binary_tree.right)
+
+            if binary_tree.left is not None: 
+                queue.append(binary_tree.left)
+
     
     def reverse(self, binary_tree):
         if not binary_tree: 
@@ -72,18 +107,26 @@ class Node:
 
 
 
-root = Node(27)
-root.add(14)
-root.add(35)
-root.add(10)
-root.add(19)
-root.add(31)
-root.add(42)
+root = Node(4)
+root.add(2)
+root.add(7)
+root.add(1)
+root.add(3)
+root.add(6)
+root.add(9)
+root.print_root()
 root.print_tree()
+
 print("")
 print("In Order",root.in_order_traversal(root))
 print("Pre Order",root.pre_order_traversal(root))
 print("Post Order",root.post_order_traversal(root))
 print("\n")
-root.reverse(root)
-root.print_tree()
+#root.reverse(root)
+#root.print_tree()
+root.printLevelOrder(root)
+#root.reverse(x)
+#root.print_tree()
+print("\n")
+
+root.printLevelOrder2(root)
