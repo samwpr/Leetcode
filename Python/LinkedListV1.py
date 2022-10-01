c11135b6ae1f4273ecde96a6814a410a0b8d009f
samwpr <swpr10x@gmail.com>
2022-10-01 14:43:10 +0800

    reattempt merge two linkedlist

--- Python/LinkedListV1.py
+++ Python/LinkedListV1.py
@@ -1,48 +1,101 @@
-from audioop import add
+from xml.etree.ElementPath import prepare_child
 
 
 class Node:
-    def __init__(self, input: int, node = None):
+    def __init__(self, input):
         self.data = input 
-        self.next = node 
+        self.next = None
 
-class SinglyLinkedList:
-    def __init__(self, root_node = None):
-        self.head = root_node
-        self.size = 0 
 
-    def add(self, input: int): 
-        new_node = Node(input, self.head)
-        self.head = new_node 
-        self.size += 1 
+class LinkedList:
+    def __init__(self):
+        self.head = None 
 
     def print(self):
-        temp_node = self.head 
-        while temp_node != None: 
-            print(temp_node.data, end="")
-            temp_node = temp_node.next 
+        temp = self.head
+        while temp != None: 
+            print(temp.data, end=" ")
+            temp = temp.next 
         print("")
 
+    def addBefore(self, input):
+        new_node = Node(input)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insertAfter(self, prevNode, input):
+        if prevNode == None: 
+            print("Previous node not found")
+            return 
+
+        new_node = Node(input)
+        new_node.next = prevNode.next 
+        prevNode.next = new_node
+
+    def append(self, input):
+        new_node = Node(input)
+        if self.head == None:
+            self.head = new_node
+            return 
+
+        dummy = self.head 
+        while dummy.next != None: 
+            dummy = dummy.next
+
+        dummy.next = new_node
+
     def reverse(self):
-        a = self.head
-        b = None 
-        c = None 
-        while a != None: 
-            b = a
-            a = a.next
-            b.next = c
-            c = b
-        self.head = b
+        curr = self.head
+        prev = None 
+        temp = None 
+        
+        while curr != None: 
+            prev = curr 
+            curr = curr.next 
+            prev.next = temp
+            temp = prev 
+        self.head = prev
+        return prev
+
+def merge(list1, list2): 
+    dummyNode = Node(0)
+    tail = dummyNode
+
+    while True:
+        if list1 is None: 
+            tail.next = list2
+            break
+        if list2 is None: 
+            tail.next = list1
+            break 
+
+        if list1.data <= list2.data: 
+            tail.next = list1
+            list1 = list1.next
+        else: 
+            tail.next = list2
+            list2 = list2.next
+        
+        tail = tail.next 
+    return dummyNode.next 
 
 
+list1 = LinkedList()
+list2 = LinkedList()
 
+list1.addBefore(1)
+list1.addBefore(5)
+list1.print()
 
-myList = SinglyLinkedList()
-myLis2 = SinglyLinkedList()
-myList.add(1)
-myList.add(2)
-myList.add(3)
-myList.print()
-myList.reverse()
-myList.print()
+list1.insertAfter(list1.head, 3)
+list1.print()
+list1.reverse()
+list1.print()
 
+list2.append(2)
+list2.append(4)
+list2.append(6)
+list2.print()
+
+list1.head = merge(list1.head, list2.head)
+list1.print()
