129a4fac4a756525b8cc0e71af4b81f3dbc7ee35
samwpr <swpr10x@gmail.com>
2022-10-03 11:40:32 +0800

    reattemp Reverse LinkedList

--- Python/Reverse_LinkedList.py
+++ Python/Reverse_LinkedList.py
@@ -6,49 +6,100 @@ Companies: Inuit, Yandex
 TODO: Reverse recursively
 '''
 
-from audioop import add
+from hashlib import new
 
 
 class Node:
-    def __init__(self, value, node = None):
-        self.value = value 
-        self.next = node 
-
+    def __init__(self, input):
+        self.data = input 
+        self.next = None 
 
 class LinkedList:
-    def __init__(self, head = None):
-        self.head = head 
-        self.size = 0
+    def __init__(self):
+        self.head = None 
     
-    def add(self, value): 
-        new_node = Node(value, self.head)
-        self.head = new_node
-        self.size += 1
-
-    def printList(self):
+    def print(self):
         temp = self.head
-        while temp != None: 
-            print(temp.value, end="")
+        while temp != None:                                     #Try while(temp)
+            print(temp.data, end=" ")
             temp = temp.next 
         print("")
 
+    def addBefore(self, input):
+        new_node = Node(input)
+        new_node.next = self.head
+        self.head = new_node
+
+    def append(self, input): 
+        new_node = Node(input)
+        
+        if self.head == None: 
+            self.head = new_node
+            return
+        
+        tail = self.head 
+        while tail.next != None:
+            tail = tail.next 
+        
+        tail.next = new_node
+
+    def insert(self, prevNode, input):
+        if prevNode == None:
+            print("Previous node not found")
+            return 
+
+        new_node = Node(input)
+        new_node.next = prevNode.next
+        prevNode.next = new_node
+
     def reverse(self):
         curr = self.head
+        prev = None 
         temp = None 
-        while curr != None: 
+        while curr != None:
             prev = curr
             curr = curr.next
-            prev.next = temp
+            prev.next = temp 
             temp = prev
         self.head = prev
         return prev
-            
 
 
-myList = LinkedList()
-myList.add(1)
-myList.add(2)
-myList.add(3)
-myList.printList()
-myList.reverse()
-myList.printList()
+def merge(list1, list2): 
+    dummy = Node(0)
+    tail = dummy
+    while True:
+        if list1 == None: 
+            tail.next = list2
+            break
+        elif list2 == None: 
+            tail.next = list1
+            break 
+
+        if list1.data <= list2.data:
+            tail.next = list1
+            list1 = list1.next 
+        else: 
+            tail.next = list2
+            list2 = list2.next 
+        tail = tail.next
+    return dummy.next
+
+
+
+list1 = LinkedList()
+list2 = LinkedList()
+list1.append(1)
+list1.append(3)
+list1.append(5)
+list1.print()
+
+list2.append(2)
+list2.append(4)
+list2.append(6)
+list2.print()
+
+list1.head = merge(list1.head, list2.head)
+list1.print()
+
+
