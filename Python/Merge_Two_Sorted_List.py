2138f31da047c4df005bd4addfb650544b5e9b95
samwpr <swpr10x@gmail.com>
2022-10-04 12:00:09 +0800

    Reattemp has cycle in linkedlist

--- Python/Merge_Two_Sorted_List.py
+++ Python/Merge_Two_Sorted_List.py
@@ -1,86 +1,106 @@
-#Make append O(1)
-#https://www.youtube.com/watch?v=l92wSWAZmnI&ab_channel=SaiAnishMalla
-
-
-from multiprocessing import dummy
-
-class Node:
+class Node: 
     def __init__(self, input):
         self.data = input 
-        self.next = None
+        self.next = None 
+
 
 class LinkedList:
     def __init__(self):
         self.head = None 
-
+    
     def print(self):
-        temp = self.head
-        while temp != None: 
-            print(temp.data, end="")
-            temp = temp.next
+        temp = self.head 
+        while temp != None:
+            print(temp.data, end=" ")
+            temp = temp.next 
         print("")
 
-    def addFront(self, input): #O(1) constant about of work 
+    def addBefore(self, input):
         new_node = Node(input)
         new_node.next = self.head
         self.head = new_node
-   
-    def append(self, input): #O(n) where n is number of nodes in the linkedlist.
+            
+    def append(self, input):
         new_node = Node(input)
         if self.head == None:
             self.head = new_node
-            return 
+            return
         
         tail = self.head
-        while tail.next != None:
+        while tail.next != None: 
             tail = tail.next 
-
         tail.next = new_node
-
-    def insertAfter(self, prev_node, input): #O(1) constant amount of work 
-        if prev_node is None: 
-            print("This is no previous node")
+    
+    def insert(self, prevNode, input):
+        if prevNode == None: 
+            print("Previous node not found")
             return 
         
         new_node = Node(input)
-        new_node.next = prev_node.next
-        prev_node.next = new_node
+        new_node.next = prevNode.next
+        prevNode.next = new_node
 
-def mergeList(headA, headB):
+    def reverse(self):
+        curr = self.head
+        temp = None 
+        while curr != None: 
+            prev = curr
+            curr = curr.next 
+            prev.next = temp 
+            temp = prev
+        self.head = prev 
+        return prev 
+
+    def hasCycle(self, head):
+        slow, fast = head, head 
+
+        while fast.next != None:
+            slow = slow.next 
+            fast = fast.next.next
+            if fast == slow:
+                return True
+        return False 
+
+
+def merge(list1, list2):
     dummyNode = Node(0)
     tail = dummyNode
-    while True: 
-        if headA is None:
-            tail.next = headB
+    while True:
+        if list1 is None:
+            tail.next = list2 
             break
-        if headB is None:
-            tail.next = headA
-            break
+        if list2 is None: 
+            tail.next = list1
+            break 
 
-        if headA.data <= headB.data:
-            tail.next = headA
-            headA = headA.next
-        else:
-            tail.next = headB
-            headB = headB.next
-        tail = tail.next
+        if list1.data <= list2.data:
+            tail.next = list1
+            list1 = list1.next
+        else: 
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next 
     return dummyNode.next
-        
+
+
+
 
 list1 = LinkedList()
-list1.append(1)
 list1.append(2)
-list1.append(3)
+list1.append(4)
+list1.append(6)
 list1.print()
+
 
 list2 = LinkedList()
-list2.addFront(1)
-list2.addFront(2)
-list2.addFront(3)
-list2.print()
-list2.insertAfter(list2.head.next, 4)
+list2.append(1)
+list2.append(2)
+list2.append(3)
 list2.print()
 
-list1.head = mergeList(list1.head, list2.head)
-list1.print()
-print("------------")
+#Creating a cycle 
+print(list1.head.next.next.next)
+list1.head.next.next.next = list1.head 
+print(list1.head.next.next.next.data)
+print(list1.hasCycle(list1.head))
+print(list2.hasCycle(list2.head))
