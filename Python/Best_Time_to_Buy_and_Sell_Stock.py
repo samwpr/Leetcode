ddf229b3197aab39fc49b7940699e26538e944f3
samwpr <swpr10x@gmail.com>
2022-10-05 12:35:03 +0800

    Reattempt: Contains Duplicate and Best Time to Buy

--- Python/Best_Time_to_Buy_and_Sell_Stock.py
+++ Python/Best_Time_to_Buy_and_Sell_Stock.py
@@ -3,22 +3,18 @@
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
 Companies: Adobe, Alation, Amazon, Apple, Atlassian, Bloomberg, ByteDance, Capital One, Cisco, Docusign, Expedia, Goldman Sachs, JP Morgan, Netflix, Orcale, Paypal, ServiceNow, Uber, Visa, Walmart, Zoho, Zoom, eBay, tcs
+prices = [7, 1, 5, 3, 6, 4] #Return 5 as maxProfit
 '''
 
 def maxProfit(prices: list) -> int:
-    profit = 0 
-    index = 0
-    for i in range(1, len(prices)):
-        if prices[i] < prices[index]:
-            index = i
-        profit = max(profit, prices[i] - prices[index])
-    return profit
-    
+    minPrice = prices[0]
+    maxProfit = 0
+    for i in range(0, len(prices)): 
+        if prices[i] < minPrice:
+            minPrice = prices[i]
+        
+        maxProfit = max(maxProfit, prices[i] - minPrice)
+    return maxProfit
 
-prices1 = [7,2,10,1,6]
-print(maxProfit(prices1))
-
-prices = [7,6,4,3,1]
+prices = [7, 1, 5, 3, 6, 4] #Return 5 as maxProfit
 print(maxProfit(prices))
-
-
--- Python/Contains_Duplicate(Easy)(Adobe).py
+++ Python/Contains_Duplicate(Easy)(Adobe).py
@@ -14,18 +14,15 @@ Output: true
 
 class Solution:
     def containsDuplicate(self, nums: list) -> bool:
-        unique = set()
+        unqiue = set()
         for i in nums: 
-            if i in unique:
-                return True
+            if i in unqiue:
+                return True 
             else:
-                unique.add(i)
+                unqiue.add(i)
         return False
 
 obj = Solution()
 nums = [1,2,3,1]
 print(obj.containsDuplicate(nums))
-nums = [1,2,3,4]
-print(obj.containsDuplicate(nums))
-nums = [1,1,1,3,3,4,3,2,4,2]
-print(obj.containsDuplicate(nums))
+
