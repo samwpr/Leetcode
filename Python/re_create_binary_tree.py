ae634010b68d6c329500800e31079ee54a4245c2
samwpr <swpr10x@gmail.com>
2022-10-08 15:01:29 +0800

    reattempt: creating binary tree

--- Python/re_create_binary_tree.py
+++ Python/re_create_binary_tree.py
@@ -1,176 +1,76 @@
-#Try this to visualize binary tree: https://www.geeksforgeeks.org/binarytree-module-in-python/
-
-'''
-In-Order - Root in the middle 
-left -> subtree -> root -> right -> subtree
-
-Pre-Order - Root at the start 
-root -> left -> subtree -> right -> subtree
-
-Post-Order - Root at the end 
-left -> subtree -> right -> subtree -> root
-
-Left will always come before right, when the root will be visited that is different 
-'''  
-
-from collections import deque
-
-class Node:
+class Node: 
     def __init__(self, input):
         self.left = None 
         self.right = None 
-        self.data = input 
+        self.root = input 
+
 
     def printRoot(self):
-        print(self.data)
+        print(self.root)
 
     def insert(self, input):
-        if self.data:
-            if input < self.data:
+        if self.root:
+            if input < self.root: 
                 if self.left is None: 
                     self.left = Node(input)
                 else: 
                     self.left.insert(input)
-            else:
-                input > self.data
-                if self.right is None:
+            else: 
+                if self.right is None: 
                     self.right = Node(input)
                 else: 
                     self.right.insert(input)
-        else:
-            self.data = input
+        else: 
+            self.root = input
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-            
-
-    def InOrder(self, root): 
-        result = []
+    def inOrder(self, root):
+        res =[]
         if root:
-            result = self.InOrder(root.left)
-            result.append(root.data)
-            result = result + self.InOrder(root.right)
-        return result
+            res = res + self.inOrder(root.left)
+            res.append(root.root)
+            res = res + self.inOrder(root.right)
+        return res 
 
+    def preOrder(self, root):
+        res = []
+        if root:
+            res.append(root.root)
+            res = res + self.preOrder(root.left)
+            res = res + self.preOrder(root.right)
+        return res 
 
-    def PreOrder(self, root):
-        result = []
+    def postOrder(self, root):
+        res = []
         if root: 
-            result.append(root.data)
-            result = result + self.PreOrder(root.left)
-            result = result + self.PreOrder(root.right)
-        return result
+            res = res + self.postOrder(root.left)
+            res = res + self.postOrder(root.right)
+            res.append(root.root)
+        return res 
 
-    def PostOrder(self, root):
-        result = []
-        if root: 
-            result = result + self.PostOrder(root.left)
-            result = result + self.PostOrder(root.right)
-            result.append(root.data)
-        return result
-
-
-    def maxDepthRecursive(self, root): #O(n) 
-        if not root:
-            return 0
-        
-        return 1 + max(self.maxDepthRecursive(root.left), self.maxDepthRecursive(root.right))
-
-
-
-    def maxDepthIterativeDFSPreOrder(self, root): #O(n) Doesn't seem to be correct 
-        stack = [[root, 1]]
-        res = 0 
-
-        while stack: 
-            node, depth = stack.pop()
-
-            if node: 
-                res = max(res, depth)
-                stack.append([node.left, depth+1])
-                stack.append([node.right, depth+1])
-        return res
-
-
-
-    def maxDepthBFS(self, root): #O(n)
-        if not root:
-            return 0
-
-        q = deque()
-        if root: 
-            q.append(root)
-
-        level = 0
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level += 1
-        return level
-
-
-def InvertTree(root):
+def invert(root):
     if not root:
         return None
-
+    
     temp = root.left 
-    root.left = root.right 
+    root.left = root.right
     root.right = temp 
 
-    InvertTree(root.left)
-    InvertTree(root.right)
+    invert(root.left)
+    invert(root.right)
     return root
-
-def PrintInOrder(root):
-    if not root:
-        return None
-
-    PrintInOrder(root.left)
-    print(root.data, end=' ')
-    PrintInOrder(root.right)
-
-
-
-root = Node(27)
-root.insert(14)
-root.insert(35)
-root.insert(10)
-root.insert(19)
-root.insert(31)
-root.insert(42)
-print("In Order",root.InOrder(root))
-print("Pre Order",root.PreOrder(root))
-print("Post Order",root.PostOrder(root))
-print("")
-PrintInOrder(root)
-InvertTree(root)
-print("")
-PrintInOrder(root)
-print("")
-print(root.maxDepthRecursive(root))
-print(root.maxDepthIterativeDFSPreOrder(root))
-print(root.maxDepthBFS(root))
-
+            
 
 
     
+tree1 = Node(5)
+tree1.printRoot()
+tree1.insert(3)
+tree1.insert(7)
+print(tree1.inOrder(tree1))
+tree1 = invert(tree1)
+print(tree1.inOrder(tree1))
+#print(tree1.preOrder(tree1))
+#print(tree1.postOrder(tree1))
 
 
-
-
-    
