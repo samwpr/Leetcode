f42cffde1f4957ff2841407114b8d5961ada6806
samwpr <swpr10x@gmail.com>
2022-10-10 12:43:42 +0800

    created binary tree from sratch

--- Python/re_create_binary_tree.py
+++ Python/re_create_binary_tree.py
@@ -1,76 +1,66 @@
-class Node: 
+class Node:
     def __init__(self, input):
-        self.left = None 
-        self.right = None 
-        self.root = input 
-
-
-    def printRoot(self):
-        print(self.root)
-
-    def insert(self, input):
+        self.left, self.right = None, None
+        self.root = input
+    
+    def add(self, input):
         if self.root:
-            if input < self.root: 
-                if self.left is None: 
+            if input < self.root:
+                if self.left is None:
                     self.left = Node(input)
-                else: 
-                    self.left.insert(input)
-            else: 
-                if self.right is None: 
+                else:
+                    self.left.add(self.left)
+            else:
+                if self.right is None:
                     self.right = Node(input)
-                else: 
-                    self.right.insert(input)
-        else: 
+                else:
+                    self.right.add(self.right)
+        else:
             self.root = input
 
-
-    def inOrder(self, root):
-        res =[]
-        if root:
-            res = res + self.inOrder(root.left)
-            res.append(root.root)
-            res = res + self.inOrder(root.right)
-        return res 
-
-    def preOrder(self, root):
+    def inOrder(self, tree):
         res = []
-        if root:
-            res.append(root.root)
-            res = res + self.preOrder(root.left)
-            res = res + self.preOrder(root.right)
+        if tree:
+            res = res + self.inOrder(tree.left)
+            res.append(tree.root)
+            res = res + self.inOrder(tree.right)
         return res 
 
-    def postOrder(self, root):
+    def preOrder(self, tree):
         res = []
-        if root: 
-            res = res + self.postOrder(root.left)
-            res = res + self.postOrder(root.right)
-            res.append(root.root)
+        if tree: 
+            res.append(tree.root)
+            res = res + self.preOrder(tree.left)
+            res = res + self.preOrder(tree.right)
         return res 
 
-def invert(root):
-    if not root:
-        return None
-    
-    temp = root.left 
-    root.left = root.right
-    root.right = temp 
+    def postOrder(self, tree):
+        res = []
+        if tree:
+            res = res + self.postOrder(tree.left)
+            res = res + self.postOrder(tree.right)
+            res.append(tree.root)
+        return res 
 
-    invert(root.left)
-    invert(root.right)
-    return root
-            
+    def invert(self, tree):
+        if not tree:
+            return None
+        
+        temp = tree.left 
+        tree.left = tree.right
+        tree.right = temp
+
+        self.invert(tree.left)
+        self.invert(tree.right)
+        return tree
 
 
     
 tree1 = Node(5)
-tree1.printRoot()
-tree1.insert(3)
-tree1.insert(7)
-print(tree1.inOrder(tree1))
-tree1 = invert(tree1)
+tree1.add(3)
+tree1.add(7)
 print(tree1.inOrder(tree1))
 #print(tree1.preOrder(tree1))
 #print(tree1.postOrder(tree1))
-
-
+tree1.invert(tree1)
+print(tree1.inOrder(tree1))
