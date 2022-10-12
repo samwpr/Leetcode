dd006f99cb436b699de051232aebc880bdeb4015
samwpr <swpr10x@gmail.com>
2022-10-12 14:50:16 +0800

    updated database

--- Python/re_create_binary_tree.py
+++ Python/re_create_binary_tree.py
@@ -1,31 +1,30 @@
-class Node:
+class Node():
     def __init__(self, input):
         self.left, self.right = None, None
         self.root = input
 
     def add(self, input):
-        if self.root:
+        if self.root: 
             if input < self.root:
                 if self.left is None:
                     self.left = Node(input)
                 else:
                     self.left.add(input)
-
             if input > self.root:
                 if self.right is None:
                     self.right = Node(input)
-                else: 
+                else:
                     self.right.add(input)
         else:
-            self.root = input 
+            self.root = input
 
     def inOrder(self, tree):
         res = []
-        if tree: 
+        if tree:
             res = res + self.inOrder(tree.left)
             res.append(tree.root)
             res = res + self.inOrder(tree.right)
-        return res
+        return res 
 
 tree1 = Node(5)
 tree1.add(2)
