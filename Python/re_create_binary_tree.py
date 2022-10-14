50f47f0e7e93f5784bb8efc50c486a779be64d0d
samwpr <swpr10x@gmail.com>
2022-10-14 14:50:37 +0800

    added: delete middle node of LinkedList

--- Python/re_create_binary_tree.py
+++ Python/re_create_binary_tree.py
@@ -3,6 +3,7 @@ class Node():
         self.left, self.right = None, None
         self.root = input
 
+
     def add(self, input):
         if self.root: 
             if input < self.root:
@@ -29,18 +30,32 @@ class Node():
     def diameter(self, tree):
         res = 0
 
+
+
+
+
+
+
+
+
+
         def dfs(tree):
             nonlocal res
 
             if not tree:
                 return 0
             
+        
+            
             left = dfs(tree.left)
-            print(tree.root)
+            print("root A",tree.root)
             print("left: ", left)
+
+            
             right = dfs(tree.right)
-            print(tree.root)
+            print("root B",tree.root)
             print('right: ', right)
+
             res = max(res, left + right)
             print('res: ', res)
             
@@ -53,10 +68,22 @@ class Node():
         dfs(tree)
         return res 
 
+
+
+
+
+
+
+
+
+
+
+
+
 tree1 = Node(5)
 tree1.add(2)
 tree1.add(7)
 tree1.add(1)
 tree1.add(3)
 print(tree1.inOrder(tree1))
-print(tree1.diameter(tree1))
+tree1.diameter(tree1)
