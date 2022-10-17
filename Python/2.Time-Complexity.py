230df507d38fb07c46c2cc3258df335ccfbf87ec
samwpr <swpr10x@gmail.com>
2022-10-17 10:15:40 +0800

    added code to test time complexity

---
+++ Python/2.Time-Complexity.py
@@ -0,0 +1,9 @@
+# https://www.pythonpool.com/python-for-loop-decrement/
+def o_of_n_1(n): #O(n)
+    for i in range(0, n):
+        print(i) #This statment will execute n times 
+
+def o_of_n_2(n): #O(n)
+    for i in range(n, 0, -1):
+        print(i) #This statment will execute n times 
+
