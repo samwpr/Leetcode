546c5fe723e95808e54c07137eb7e1abaa84e1e6
samwpr <swpr10x@gmail.com>
2022-10-30 15:55:24 +0800

    reattempt

--- Python/0.whiteboard.py
+++ Python/0.whiteboard.py
@@ -1,23 +1,8 @@
-def containsDuplicate2(nums, k):
-    unique = set()
-    duplicateNum = None
-    duplicateIndex = None
-    for index, element in enumerate(nums):
-        if element in unique:
-            duplicateNum = element
-            duplicateIndex = index
-        else:
-            unique.add(element)
+import heapq
 
-    indexOfFirst = nums.index(duplicateNum)
-    print(indexOfFirst)
-    j = abs(indexOfFirst - duplicateIndex)
-    print(j)
+#Difference between heap and binary tree 
 
-    return abs(indexOfFirst - duplicateIndex) <= k
+'''
+heap visualised 
+'''
 
-
-
-
-print(containsDuplicate2([1, 0, 1, 1], 1)) #True
-#print(containsDuplicate2([1, 2, 3, 1], 3)) #True
